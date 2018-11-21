eb59e62c732c9486050e3f69c6deee44944990aa
bahadasx <sbahadaran@industrydive.com>
2018-11-21 17:23:01 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='data wrangling tools and techniques to use with lytics profile data',
+    author='Sasan Bahadaran',
+    license='MIT',
+)
